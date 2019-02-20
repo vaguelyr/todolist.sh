1dcabf81a6c78dbe91bb61f65d3ff2014cbed74c
vaguelyr <greymoth90@gmail.com>
2019-02-20 09:22:51 -0600

    added priority coloring

--- todolist.sh
+++ todolist.sh
@@ -7,20 +7,20 @@
 
 # colors
 nc='\033[0m'
-color_array[0]='\033[0m'        # none
-color_array[1]='\033[0;30m'     # black
-color_array[2]='\033[1;30m'     # dgrey
-color_array[3]='\033[0;31m'     # red
-color_array[4]='\033[1;31m'     # lred
-color_array[5]='\033[0;32m'     # green
-color_array[6]='\033[1;32m'     # lgreen
-color_array[7]='\033[0;33m'     # brown
-color_array[8]='\033[1;33m'     # yellow
-color_array[9]='\033[0;34m'     # blue
-color_array[10]='\033[1;34m'    # lblue
-color_array[11]='\033[0;35m'    # purple
+color_array[0]='\033[0m' 	# none
+color_array[1]='\033[0;30m' 	# black
+color_array[2]='\033[1;30m' 	# dgrey
+color_array[3]='\033[0;31m' 	# red
+color_array[4]='\033[1;31m' 	# lred
+color_array[5]='\033[0;32m' 	# green
+color_array[6]='\033[1;32m' 	# lgreen
+color_array[7]='\033[0;33m' 	# brown
+color_array[8]='\033[1;33m' 	# yellow
+color_array[9]='\033[0;34m'	# blue
+color_array[10]='\033[1;34m'  	# lblue
+color_array[11]='\033[0;35m'  	# purple
 color_array[12]='\033[38;5;127m' # lpurple
-color_array[12]='\033[1;35m'    # lpurple
+color_array[12]='\033[1;35m' 	# lpurple
 color_array[13]='\033[0;36m'    # cyan
 color_array[14]='\033[1;36m'    # lcyan
 color_array[15]='\033[0;37m'    # lgrey
@@ -29,7 +29,12 @@ color_array[17]='\033[38;5;239' # birthdays
 color_array[18]='\033[38;5;69m' # hw
 color_array[19]='\033[38;5;30m' # fainter cyan
 
-
+# priorities
+priority[0]="${color_array[4]}" # tests
+priority[1]="${color_array[18]}" # homework 
+priority[2]="${color_array[8]}" # life stuff? 
+priority[3]="${color_array[0]}" # birthdays 
+priority[4]="${color_array[2]}" # academics 
 
 # functions
 todoWrite(){
@@ -39,8 +44,11 @@ todoWrite(){
 	# colors
 	# check if the 3rd arg is a number and then set the color
 	echo $3 | grep -P "[0-9]{1,2}" >/dev/null && {
-		#linecolor="\033[${color_array[$3]}m"
-		linecolor="${color_array[$3]}"
+		if [ "$3" -gt 4 ] ; then
+			linecolor="${color_array[$3]}"
+		else
+			linecolor="${priority[$3]}"
+		fi
 
 		# delete the 3rd element since its a color number
 		# and add the rest of the line to the msg
@@ -64,7 +72,6 @@ todoWrite(){
 	# write to txt
 	echo -e $ds,${linecolor}$msg${nc} >> $logfile
 }
-
 todoDisplay(){
 	now=$(date +%s)
 	echo todoDisplay now is $now $(date +%c)
@@ -96,12 +103,13 @@ csvfile='./todo.csv'
 rm $logfile
 rm $csvfile
 
-# =============================== color test
+# =============================== priority test
 todoWrite "feb 01 2035 12:00:00" "color" 0 0
 todoWrite "feb 02 2035 12:00:00" "color" 1 1
 todoWrite "feb 03 2035 12:00:00" "color" 2 2
 todoWrite "feb 04 2035 12:00:00" "color" 3 3
 todoWrite "feb 05 2035 12:00:00" "color" 4 4 
+# =============================== color test
 todoWrite "feb 06 2035 12:00:00" "color" 5 5 
 todoWrite "feb 07 2035 12:00:00" "color" 6 6
 todoWrite "feb 08 2035 12:00:00" "color" 7 7 
